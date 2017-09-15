4ced72b0d7eb20c73c44ba0a875ef01493440de1
ANG1170\Vu Xuan Luyen <luyenvu8x@gmail.com>
2017-09-15 17:50:14 +0700

    Searching and binding in angular

--- TeduShop.Web/app/components/product_categories/productCategoryListController.js
+++ TeduShop.Web/app/components/product_categories/productCategoryListController.js
@@ -9,10 +9,19 @@
         $scope.pagesCount = 0;
         $scope.getProductCagories = getProductCagories;
 
+        $scope.keyword = '';
+
+        $scope.search = search;
+
+        function search() {
+            getProductCagories();
+        }
+
         function getProductCagories(page) {
             page = page || 0;
             var config = {
                 params: {
+                    keyword: $scope.keyword,
                     page: page,
                     pageSize: 2
                 }

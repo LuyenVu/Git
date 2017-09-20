8643d76398ec38484077d211dacee6cf99d84e98
ANG1170\Vu Xuan Luyen <luyenvu8x@gmail.com>
2017-09-20 22:06:14 +0700

    Edit Category product in ng

--- TeduShop.Web/Api/ProductCategoryController.cs
+++ TeduShop.Web/Api/ProductCategoryController.cs
@@ -16,13 +16,17 @@ namespace TeduShop.Web.Api
     [RoutePrefix("api/productcategory")]
     public class ProductCategoryController : ApiControllerBase
     {
-        private IProductCategoryService _productCategoryService;
+        #region initialize 
 
+        private IProductCategoryService _productCategoryService;
         public ProductCategoryController(IErrorService errorService, IProductCategoryService productCategoryService)
             : base(errorService)
         {
             this._productCategoryService = productCategoryService;
         }
+        
+        #endregion
+
         [Route("getallparents")]
         [HttpGet]
         public HttpResponseMessage GetAll(HttpRequestMessage request)
@@ -64,6 +68,22 @@ namespace TeduShop.Web.Api
             });
         }
 
+        [Route("getbyId/{id:int}")]
+        [HttpGet]
+        public HttpResponseMessage GetById(HttpRequestMessage request,int id)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                var model = _productCategoryService.GetById(id);
+
+                var responseData = Mapper.Map<ProductCategory, ProductCategoryViewModel>(model);
+
+                var response = request.CreateResponse(HttpStatusCode.OK, responseData);
+
+                return response;
+            });
+        }
+
 
         [Route("create")]
         [HttpPost]
@@ -81,6 +101,7 @@ namespace TeduShop.Web.Api
                 {
                     var newProductCategory = new ProductCategory();
                     newProductCategory.UpdateProductCategory(productCategoryVm);
+                    newProductCategory.CreatedDate = DateTime.Now;
 
                     _productCategoryService.Add(newProductCategory);
                     _productCategoryService.Save();
@@ -92,5 +113,36 @@ namespace TeduShop.Web.Api
                 return response;
             });
         }
+
+        [Route("update")]
+        [HttpPut]
+        [AllowAnonymous]
+        public HttpResponseMessage Update(HttpRequestMessage request, ProductCategoryViewModel productCategoryVm)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (!ModelState.IsValid)
+                {
+                    response = request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var dbProductCategory = _productCategoryService.GetById(productCategoryVm.ID);
+                    dbProductCategory.UpdateProductCategory(productCategoryVm);
+                    dbProductCategory.UpdatedDate = DateTime.Now;
+
+                    _productCategoryService.Update(dbProductCategory);
+                    _productCategoryService.Save();
+
+                    var responseData = Mapper.Map<ProductCategory, ProductCategoryViewModel>(dbProductCategory);
+                    response = request.CreateResponse(HttpStatusCode.Created, responseData);
+                }
+
+                return response;
+            });
+        }
+
+
     }
 }

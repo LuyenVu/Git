6f1cec1bb04762c471762612c712563c376e0263
ANG1170\Vu Xuan Luyen <luyenvu8x@gmail.com>
2017-09-27 13:22:45 +0700

    logon administration page and authorize user login

--- TeduShop.Web/app/components/login/loginController.js
+++ TeduShop.Web/app/components/login/loginController.js
@@ -1,9 +1,22 @@
 ﻿(function (app) {
-    app.controller('loginController', loginController);
-    loginController.$inject = ['$scope', '$state'];
-    function loginController($scope, $state) {
-        $scope.loginSubmit = function () {
-            $state.go('home');
-        }
-    }
+    app.controller('loginController', ['$scope', 'loginService', '$injector', 'notificationService',
+        function ($scope, loginService, $injector, notificationService) {
+
+            $scope.loginData = {
+                userName: "",
+                password: ""
+            };
+
+            $scope.loginSubmit = function () {
+                loginService.login($scope.loginData.userName, $scope.loginData.password).then(function (response) { // 25-Sep-2017: instate 'then' by 'finally' 
+                    if (response != null && response.error == undefined) { // !=   instate of ==
+                        notificationService.displayError("Đăng nhập không đúng.");
+                    }
+                    else {
+                        var stateService = $injector.get('$state');
+                        stateService.go('home');
+                    }
+                });
+            }
+        }]);
 })(angular.module('tedushop'));
--- TeduShop.Web/app/shared/services/apiService.js
+++ TeduShop.Web/app/shared/services/apiService.js
@@ -3,16 +3,17 @@
 (function (app) {
     app.factory('apiService', apiService);
 
-    apiService.$inject = ['$http', 'notificationService'];
+    apiService.$inject = ['$http', 'notificationService', 'authenticationService'];
 
-    function apiService($http, notificationService) {
+    function apiService($http, notificationService, authenticationService) {
         return {
             get: get,
             post: post,
             put: put,
-            del:del
+            del: del
         }
         function del(url, data, success, failure) {
+            authenticationService.setHeader();
             $http.delete(url, data).then(function (result) {
                 success(result);
             }, function (error) {
@@ -26,8 +27,8 @@
 
             });
         }
-
         function post(url, data, success, failure) {
+            authenticationService.setHeader();
             $http.post(url, data).then(function (result) {
                 success(result);
             }, function (error) {
@@ -41,8 +42,8 @@
 
             });
         }
-
         function put(url, data, success, failure) {
+            authenticationService.setHeader();
             $http.put(url, data).then(function (result) {
                 success(result);
             }, function (error) {
@@ -56,9 +57,8 @@
 
             });
         }
-
-
         function get(url, params, success, failure) {
+            authenticationService.setHeader();
             $http.get(url, params).then(function (result) {
                 success(result);
             }, function (error) {

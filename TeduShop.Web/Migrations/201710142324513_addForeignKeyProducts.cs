22a453d3acec24a4732669a67a1440f2986724f3
ANG1170\Vu Xuan Luyen <luyenvu8x@gmail.com>
2017-10-15 09:29:12 +0700

    Tag for products

---
+++ TeduShop.Web/Migrations/201710142324513_addForeignKeyProducts.cs
@@ -0,0 +1,16 @@
+namespace TeduShop.Web.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addForeignKeyProducts : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
